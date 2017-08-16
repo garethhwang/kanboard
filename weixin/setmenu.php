809915e30de569e0c38ea8760b2c2ee44bcfd10d
hwang <hh698@126.com>
2017-08-16 17:34:47 +0800

    weixin

--- weixin/setmenu.php
+++ weixin/setmenu.php
@@ -11,11 +11,14 @@ $weixin = new weixinapi();
 $scope = "snsapi_userinfo";
 $state = "ggzs";
 
-$button[] = array('type' => "view",
-       'name' => "ggzs",
+$button = array(
+    array('type' => "view",
+        'name' => "ggzs",
         //'name' => json_encode('工程管理助手',JSON_UNESCAPED_UNICODE),
         'url'  => $weixin->oauth2_authorize("m.jinxingjk.com", $scope, $state)
-    );
+    )
+
+);
 
 $result = $weixin->create_menu($button);
 var_dump($result);
